e7b2f971d7395b9b4de25d00c3d40428cd44cf20
Shubham Mandloi <46235554+shubhamMandloi@users.noreply.github.com>
2021-06-22 14:41:10 +0530

    Add files via upload

---
+++ try/src/javaPrgm/DupFiles.java
@@ -0,0 +1,89 @@
+package javaPrgm;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DupFiles {
+
+    public static void main(String[] args) {
+        String baseDirPath = "E:\\Pictures";
+        String[] extensions = {"png", "jpg", "jpeg", "mp4", "3gp", "mov", "dng"};
+
+
+        try {
+            HashMap<String, String> uniqueFileMap = new HashMap<>();
+            HashMap<String, String> dupFileMap = new HashMap<>();
+            List<String> fileList = new ArrayList<>();
+
+            List<String> files = findFiles(Paths.get(baseDirPath), extensions);
+            files.forEach(x -> fileList.add(x));
+           /* System.out.println(fileList.size());
+            fileList.forEach(x -> System.out.println(x));*/
+
+            for (String filePath : fileList) {
+                String fileName = filePath.substring(filePath.lastIndexOf("\\") + 1);
+                String folderName = filePath.substring(0, filePath.lastIndexOf("\\"));
+                if (uniqueFileMap.containsKey(fileName)) {
+
+                        dupFileMap.put(fileName, uniqueFileMap.get(fileName) + ", "+folderName +","+ dupFileMap.getOrDefault(fileName, " "));
+
+                }else
+                    uniqueFileMap.put(fileName,folderName);
+
+
+
+            }
+            System.out.println("Unique file size : "+uniqueFileMap.size());
+            System.out.println("Duplicate file size : "+dupFileMap.size());
+            dupFileMap.forEach((k, v) -> System.out.println("Duplicate  file :   " + k + " and folder is " + v));
+
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static List<String> findFiles(Path path, String[] fileExtensions) throws IOException {
+
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory!");
+        }
+
+        List<String> result;
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk
+                    .filter(p -> !Files.isDirectory(p))
+                    // convert path to string
+                    .map(p -> p.toString().toLowerCase())
+                    .filter(f -> isEndWith(f, fileExtensions))
+                    .collect(Collectors.toList());
+        }
+        return result;
+
+    }
+
+    private static boolean isEndWith(String file, String[] fileExtensions) {
+        boolean result = false;
+        for (String fileExtension : fileExtensions) {
+            if (file.endsWith(fileExtension)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
